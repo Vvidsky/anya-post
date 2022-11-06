ff0b182159e2c0e56e62718cf9d80627847d154e
Vvidsky <kulawutm.developer@gmail.com>
2022-11-06 17:27:46 +0700

    Added route error handling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import {Routes, Route} from 'react-router-dom';
-import Home from './routes/Home';
-import Layout from './routes/Layout/Layout';
-import Index from './routes/index/Index'
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./routes/Home";
+import Layout from "./routes/Layout/Layout";
+import Index from "./routes/index/Index";
+import News from "./routes/news/news";
+import "./App.css";
 
 function App() {
   return (
     <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/news" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path=":newsId" elements={<div>newsid</div>} />
-        </Route>
-      </Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/news/:newsId" element={<News />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Route>
+    </Routes>
   );
 }
 
