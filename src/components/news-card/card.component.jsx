f3b9f18a76f26f940e3cd1810dc92421c3182a9a
Vvidsky <kulawutm.developer@gmail.com>
2022-11-22 00:35:47 +0700

    Display publisher

--- src/components/news-card/card.component.jsx
+++ src/components/news-card/card.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './card.component.css'
 import { FontAwesomeIcon }  from '@fortawesome/react-fontawesome'
 import { faStar, faFeather } from '@fortawesome/free-solid-svg-icons'
-import  crunchyroll_logo  from '../../assets/crunchyroll_logo.png'
+import crunchyroll_logo from '../../assets/crunchyroll_logo.png'
 import CBR_logo  from '../../assets/cbr_logo.png'
 import animeGeek_logo from '../../assets/animeGeek_logo.png'
 
@@ -26,16 +26,16 @@ function Card({ news, searchQuery }) {
   }
 
   function displayLogo(publisher) {
-    const selectorElement = document.getElementById("logo_img");
     // Conditional Images (logo)
+    let selectedImage;
     if (publisher === "Crunchyroll") {
-      selectorElement.innerHTML = <img src={crunchyroll_logo} alt="crunchyroll_logo" />;
+      selectedImage = crunchyroll_logo;
     } else if (publisher === "CBR") {
-      selectorElement.innerHTML = <img src={CBR_logo} alt="CBR_logo" />
+      selectedImage = CBR_logo;
     } else if (publisher === "Anime Geek") {
-      selectorElement.innerHTML = <img src={animeGeek_logo} alt="animeGeek_logo" />;
+      selectedImage = animeGeek_logo;
     }
-    return selectorElement;
+    return <img src={selectedImage} className="img-fluid" style={{maxWidth:"100px", float:"right"}} alt="crunchyroll_logo" />;
   }
 
   return (
@@ -61,10 +61,12 @@ function Card({ news, searchQuery }) {
       </div>
 
       <div className="card-footer">
-        <p className="text-muted"><FontAwesomeIcon className="star-icon" icon={faStar} />Score {news._score}</p>
-        <div id="logo_img">
-          { displayLogo(data.publisher) }
-        </div>
+        <p className="text-muted"><FontAwesomeIcon className="d-inline star-icon" icon={faStar} />Score {news._score}
+          <div id="logo_img" className="d-inline">
+          { displayLogo(data.Publisher) }
+          </div>
+        </p>  
+        
       </div>
     </div>
   )
